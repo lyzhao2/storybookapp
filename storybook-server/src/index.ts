52554d3e0b1e1c27c9ce5eaeadc6859046ad8c73
Linda <lyzhao2@wisc.edu>
2024-04-17 17:42:16 -0700

    staging

--- storybook-server/src/index.ts
+++ storybook-server/src/index.ts
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.post("/api/storybook", async (req, res) => {
-    const { llm_choice, dalle_choice, story} = req.body;
+    // const { llm_choice, dalle_choice, story} = req.body;
     const completion = await openai.chat.completions.create({
         messages: [{"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": "Who won the world series in 2020?"},
@@ -20,8 +20,7 @@ app.post("/api/storybook", async (req, res) => {
             {"role": "user", "content": "Where was it played?"}],
         model: "gpt-3.5-turbo",
     });
-    console.log(completion.choices[0]);
-    res.end();
+    console.log("openai response: " + completion.choices[0]);
 })
 
 app.listen(5000, () => {
